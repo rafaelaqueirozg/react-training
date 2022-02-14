03660f27178a428474c5868a6d4a1cb25302f257
rafaelaqueirozg <rqueiroz2108@gmail.com>
2022-02-14 13:54:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-3g3wq7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3g3wq7)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+const nome = 'visitante';
+
+  return (
+    <div>
+      <h1>Olá, {nome}!</h1>
+      <p>Insira seu nome abaixo</p>
+      <input />
+    </div>
+  );
+}
