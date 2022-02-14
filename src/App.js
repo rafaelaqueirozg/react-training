e99378665f98871963c1778e07a8d236ecb72c61
rafaelaqueirozg <rqueiroz2108@gmail.com>
2022-02-14 15:35:42 -0300

    looping em arrays e use state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
 export default function App() {
-const nome = 'visitante';
+  let name = 'visitante';
+
+  const [nome, setNome] = useState('visitante state');
+
+  const mudaCampo = (novoNome) => {
+    name = novoNome;
+    console.log(name);
+    console.log('teste', novoNome);
+    setNome(novoNome);
+  };
+  //
+  const arrayDeNumeros = [1, 2, 3, 4, 5];
+
+  const arrayDePessoas = [
+    { nome: 'Gab', idade: 20 },
+    { nome: 'rafa', idade: 18 },
+  ];
+
+  const varreArray = () => {
+    let array1 = [];
+    let array2 = [];
+    let idade = 0;
+
+    array1 = arrayDePessoas.forEach((elemento) => {
+      idade += elemento.idade;
+      return elemento.idade;
+    });
+
+    array2 = arrayDePessoas.map((elemento) => {
+      return elemento.idade;
+    });
+
+    console.log('array1', array1);
+    console.log('array2', array2);
+  };
 
   return (
-    <div>
+    <>
       <h1>Olá, {nome}!</h1>
-      <p>Insira seu nome abaixo</p>
-      <input />
-    </div>
+      <p>Insira seu nome abaixo e veja a mágica acontecer...</p>
+      <input
+        placeholder="digite seu nome aqui"
+        onChange={(evento) => mudaCampo(evento.target.value)}
+      />
+      <br />
+      {arrayDeNumeros.map((numero) => (
+        <>{numero}</>
+      ))}
+    </>
   );
 }
